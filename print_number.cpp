ce3b6217286043d84b86ba8fb2c23b115c893435
bandigirijadevi <girijadevibandi@gmail.com>
2025-12-29 19:27:32 +0530

    hello world

---
+++ print_number.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+int number;
+cout << "Enter a integer : ";
+cin >> number;
+ 
+cout << "you entered" << number;
+return 0;
+}
---
+++ sum_of_two_integers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int first_number,second_number,sum;
+    cout << "Enter the two Integers :";
+    cin >> first_number >> second_number;
+    sum = first_number +second_number;
+    cout << first_number << "+" << second_number "=" << sum;
+    return 0;
+}
